7e61bd965fe559a9fbc485ad909d31131902f2cc
ngyingxue <95698021+ngyingxue@users.noreply.github.com>
2021-12-25 21:29:15 +0800

    Update fantasy-game-inventory.py

--- fantasy-game-inventory.py
+++ fantasy-game-inventory.py
@@ -14,6 +14,14 @@ Inventory:
 Total number of items: 62
 
 Hint: You can use a for loop to loop through all the keys in a dictionary.
+
+List to Dictionary Function for Fantasy Game Inventory
+Imagine that a vanquished dragon’s loot is represented as a list of strings like this:
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+Write a function named addToInventory(inventory, addedItems), where the inventory parameter is a dictionary representing the player’s
+inventory (like in the previous project) and the addedItems parameter is a list like dragonLoot. The addToInventory() function should return a
+dictionary that represents the updated inventory. Note that the addedItems list can contain multiples of the same item.
 '''
 
 # inventory.py
